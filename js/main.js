963dac42bf09893228647cc820f027f51a98c6f7
camario25 <camario25@yahoo.com>
2018-02-06 16:19:05 -0800

    error messages added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
+$(document).ready(function () {
+
+console.log("hello");
+
 $('form').on('submit', function (e) {
+  console.log('funtion1')
   e.preventDefault();
-  $('form input, form textarea').each(function () {
+  console.log(e);
+  $('form input, textarea').each(function () {
     var $emptyElement = $(this);
-    var $typeElement = emptyElement.attr('type');
+    // var typeElement = emptyElement.attr('type');
     
     if ($emptyElement.val() === "") {
-      $emptyElement.addClass("error");
-     $emptyElement.siblings('error-message').fadeIn();
+      $emptyElement.attr("class", "error");
+      console.log($emptyElement);
+     $emptyElement.siblings('.errorMessage').fadeIn();
      return;
     }
+    $emptyElement.removeClass('error');
+    $emptyElement.siblings('.errorMessage').hide();
+    // if ('form input').on('focus', function(e) {
+    //   $emptyElement.attr('class', 'inputFilled');
+    // });
   });
+});
 });
